af4cd7b5ad6d4a9a57b3ecd1e87aa2fbd69407ab
nezhinsky <m.nezhinsky@yandex.ru>
2023-01-14 16:02:23 +0400

    update workflow

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -6,6 +6,6 @@ class TestSettings:
     def test_settings(self):
 
         assert not settings.DEBUG, 'Проверьте, что DEBUG в настройках Django выключен'
-        assert settings.DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql', (
+        assert settings.DATABASES['default']['ENGINE'] == 'django.db.backends.sqlite3', (
             'Проверьте, что используете базу данных postgresql'
         )
